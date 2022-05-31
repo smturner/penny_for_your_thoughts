dc3cbd544e63b21093a914bec4fb2b19ef867b68
Alex Lane <97alane@gmail.com>
2022-05-31 10:58:28 -0500

    basic nav setup

---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import {Navbar, Nav, Container} from 'react-bootstrap'
+
+const Navbar = () => {
+    return (
+        <header>
+            <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="/">Penny For Your Thoughts</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/">Creat Quotes</Nav.Link>
+        <Nav.Link href="/">View Quotes</Nav.Link>
+        <Nav.Link href="/">Your Quotes</Nav.Link>
+        
+
+        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown> */}
+
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </header>
+    );
+};
+
+export default Navbar;
