ed4bdfaddadc8f5e8f22167b8f3570d88ad5a3c2
Sarah Turner <turnersarah228@gmail.com>
2022-05-31 13:38:24 -0500

    fixed query quote and user

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,25 +1,2 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        userName
-      }
-    }
-  }
-`
-
-export const ADD_USER = gql `
-mutation addUser($userName: String!, $email: String!, $password: String!) {
-    addUser(userName: $userName, email: $email, password: $password) {
-      token
-      user {
-        _id
-        userName
-      }
-    }
-  }
-`
