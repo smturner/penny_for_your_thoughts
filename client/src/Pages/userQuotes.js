3e42cf3a1b85061f835f2f14611e162c3b39977f
Rhoward1 <ryanmhoward@gmail.com>
2022-06-01 11:49:39 -0500

    added more place holders for userQuotes

--- client/src/Pages/userQuotes.js
+++ client/src/Pages/userQuotes.js
@@ -34,6 +34,38 @@ const UserQuotes = () => {
                     </blockquote>
                 </Card.Body>
             </Card>
+
+            <Card>
+                <Card.Header>Quote</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {' '}
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.{' '}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+
+            <Card>
+                <Card.Header>Quote</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {' '}
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.{' '}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
         </div>
   
     )
