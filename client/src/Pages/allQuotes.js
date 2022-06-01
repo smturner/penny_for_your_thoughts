88c0130b648c72bc61e85e9d5d334b8403cc3399
Rhoward1 <ryanmhoward@gmail.com>
2022-06-01 11:45:01 -0500

    added page, allQuotes

--- client/src/Pages/allQuotes.js
+++ client/src/Pages/allQuotes.js
@@ -1,2 +1,74 @@
 import React from 'react';
 
+const AllQuotes = () => {
+    return (
+        <div>
+            <Card>
+                <Card.Header>Quote</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {' '}
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.{' '}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+  
+            <Card>
+                <Card.Header>Quote</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {' '}
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.{' '}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+  
+            <Card>
+                <Card.Header>Quote</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {' '}
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.{' '}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+  
+            <Card>
+                <Card.Header>Quote</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <p>
+                            {' '}
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                            erat a ante.{' '}
+                        </p>
+                        <footer className="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+        </div>
+  
+    )
+  }
+  
+  export default AllQuotes;
