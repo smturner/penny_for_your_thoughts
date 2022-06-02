5113ad360db7371c03034879f3749f2c4296d143
Sarah Turner <turnersarah228@gmail.com>
2022-06-02 12:05:11 -0500

    changes

--- client/src/Pages/userQuotes.js
+++ client/src/Pages/userQuotes.js
@@ -1,64 +1,24 @@
 import React from 'react';
-
-import { Card, Form, Button } from 'react-bootstrap';
+import { useQuery } from '@apollo/client';
+import QuoteList from '../components/QuoteList';
+import { QUERY_QUOTES } from '../utils/queries';
+import { Card, Form, Button} from 'react-bootstrap';
+import MainNav from '../components/navbar/navbar';
 
 const UserQuotes = () => {
+    const { loading, data } = useQuery(QUERY_QUOTES);
+    const quotes= data?.quotes || [];
+
     return (
         <div>
-            <Card className="text-dark mb-3">
-                <Card.Header>Quote</Card.Header>
-                <Card.Body>
-                    <blockquote className="blockquote mb-0">
-                        <p>
-                            {' '}
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                            erat a ante.{' '}
-                        </p>
-                        <footer className="blockquote-footer">
-                            Someone famous in <cite title="Source Title">Source Title</cite>
-                        </footer>
-                        <Button variant="outline-secondary">Edit</Button>{' '}
-                        <Button variant="outline-danger">Delete</Button>{' '}
-                    </blockquote>
-                </Card.Body>
-            </Card>
+            <MainNav />
+           
+            <div className="col-12 col-md-8 mb-3">
+                <QuoteList
+                 />
+            </div>
 
-            <Card className="text-dark mb-3">
-                <Card.Header>Quote</Card.Header>
-                <Card.Body>
-                    <blockquote className="blockquote mb-0">
-                        <p>
-                            {' '}
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                            erat a ante.{' '}
-                        </p>
-                        <footer className="blockquote-footer">
-                            Someone famous in <cite title="Source Title">Source Title</cite>
-                        </footer>
-                        <Button variant="outline-secondary">Edit</Button>{' '}
-                        <Button variant="outline-danger">Delete</Button>{' '}
-                    </blockquote>
-
-                </Card.Body>
-            </Card>
-
-            <Card className="text-dark mb-3">
-                <Card.Header>Quote</Card.Header>
-                <Card.Body>
-                    <blockquote className="blockquote mb-0">
-                        <p>
-                            {' '}
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                            erat a ante.{' '}
-                        </p>
-                        <footer className="blockquote-footer">
-                            Someone famous in <cite title="Source Title">Source Title</cite>
-                        </footer>
-                        <Button variant="outline-secondary">Edit</Button>{' '}
-                        <Button variant="outline-danger">Delete</Button>{' '}
-                    </blockquote>
-                </Card.Body>
-            </Card>
+          
 
 
         </div>
---
+++ client/src/components/QuoteList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, Form, Button} from 'react-bootstrap';
+
+    const Quote= () =>{
+    return(
+    <Card className= "text-dark mb-3">
+    <Card.Header>Quote</Card.Header>
+    <Card.Body>
+        <blockquote className="blockquote mb-0">
+            <p>
+                {' '}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                erat a ante.{' '}
+            </p>
+            <footer className="blockquote-footer">
+                Someone famous in <cite title="Source Title">Source Title</cite>
+            </footer>
+            <Button variant="outline-secondary">Edit</Button>{' '}
+            <Button variant="outline-danger">Delete</Button>{' '}
+        </blockquote>
+    </Card.Body>
+    </Card>
+    )
+    }
+
+    export default Quote
