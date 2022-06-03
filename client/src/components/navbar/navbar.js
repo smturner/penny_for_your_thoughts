be1a30555c5e392dcc50dded80554651f59c1002
Sarah Turner <turnersarah228@gmail.com>
2022-06-03 07:37:17 -0500

    switching branches

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -18,14 +18,6 @@ const MainNav = () => {
         
 
 
-              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown> */}
-
             </Nav>
           </Navbar.Collapse>
         </Container>
