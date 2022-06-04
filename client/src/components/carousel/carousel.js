86c1567c925003856e8572bcdf2c64473d403ecc
Rhoward1 <ryanmhoward@gmail.com>
2022-06-03 23:17:20 -0500

    addimages to carousel and changed transition

--- client/src/components/carousel/carousel.js
+++ client/src/components/carousel/carousel.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import {Carousel, CarouselGroup} from 'react-bootstrap'
 import Penny from '../../images/penny3.jpg'
+import Penny1 from '../../images/penny4.jpg'
+import Penny2 from '../../images/penny5.jpg'
+
 
 function HomeCarousel() {
     const [index, setIndex] = useState(0);
@@ -10,41 +13,43 @@ function HomeCarousel() {
     };
   
     return (
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
+      // <Carousel activeIndex={index} onSelect={handleSelect}>
+      //   <Carousel.Item>
+      <Carousel fade>
+  <Carousel.Item interval={1200}>
           <img
             className="d-block h-25! w-100"
             src={Penny}
             alt="First slide"
           />
           <Carousel.Caption >
-            <h3>"Users are not good people"</h3>
-            <p>-Poornima</p>
+            <h3></h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block h-25 w-100"
-            src={Penny}
+            src={Penny1}
             alt="Second slide"
           />
   
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3></h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block h-25 w-100"
-            src={Penny}
+            src={Penny2}
             alt="Third slide"
           />
   
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3></h3>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              
             </p>
           </Carousel.Caption>
         </Carousel.Item>
